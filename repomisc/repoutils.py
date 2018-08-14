8a2e7e2b0ab76a354efbf77be7b4cdcb666bdd7a
miacro <fqguozhou@gmail.com>
2018-08-14 16:58:51 +0800

    repo.urlparse complete

--- repomisc/repoutils.py
+++ repomisc/repoutils.py
@@ -14,43 +14,54 @@ class Repo():
     def __repr__(self):
         return repr(vars(self))
 
-    def urlparse(self, repourl):
+    def __setattr__(self, name, value):
+        if name == "reponame" and value and value[-4:] == ".git":
+            value = value[:-4]
+        return super().__setattr__(name, value)
 
-        attr_patterns = {
-            "scheme": "(http[s]?)|(ssh)|(ftp)|(file)",
+    def urlparse(self, repourl):
+        patterns = {
+            "scheme": "(http[s]?)|(ssh)|(ftp[s]?)|(git)",
             "username": "[^/:@]+",
             "password": "[^/:@]+",
             "hostname": "[^/:@]+",
             "port": "[0-9]{1,6}",
             "owner": "[^/]+",
-            "reponame": "[^/]+[^(.git)]",
+            "reponame": "[^/]+",
+            "dirname": "([^/]+)",
         }
-        patterns = {
-            "basic":
-            "^{}://({}(:{})?@)?{}(:{})?/{}/{}(.git)?$".format(
-                *[
-                    "(?P<{}>{})".format(attrname, attr_patterns[attrname])
-                    for attrname in ("scheme", "username", "password",
-                                     "hostname", "port", "owner", "reponame")
-                ]),
-            "file":
-            "{}:///({}/)+{}(.git)?/?",
-        }
-        parseresult = re.fullmatch(patterns["basic"], repourl)
 
-        for name in vars(self).keys():
-            setattr(self, name, parseresult.group(name))
+        def pattern_groups(names):
+            if isinstance(names, str):
+                return "(?P<{}>{})".format(names, patterns[names])
+            return [
+                "(?P<{}>{})".format(name, patterns[name]) for name in names
+            ]
 
+        patterns["uri"] = "^{}://({}(:{})?@)?{}(:{})?(/{})*/{}/{}/?$".format(
+            pattern_groups("scheme"), pattern_groups("username"),
+            pattern_groups("password"), pattern_groups("hostname"),
+            pattern_groups("port"), patterns["dirname"],
+            pattern_groups("owner"), pattern_groups("reponame"))
+        patterns["scp"] = "^({}@)?{}:/?({}/)*{}/{}/?$".format(
+            pattern_groups("username"), pattern_groups("hostname"),
+            patterns["dirname"], pattern_groups("owner"),
+            pattern_groups("reponame"))
+        patterns["file"] = "^((file://)?/)?({}/)*{}/?$".format(
+            patterns["dirname"], pattern_groups("reponame"))
 
-if __name__ == "__main__":
-    repo = Repo()
-    repo.urlparse("https://a:b@github.com/miacro/mlmisc")
-    print(repo)
-    repo.urlparse("http://a:b@github.com/miacro/mlmisc")
-    print(repo)
-    repo.urlparse("https://a:b@github.com/miacro/mlmisc.git")
-    print(repo)
-    repo.urlparse("https://github.com/miacro/mlmisc")
-    print(repo)
-    repo.urlparse("https://github.com:12345/miacro/mlmisc.git")
-    print(repo)
+        parseresult = re.fullmatch(patterns["uri"], repourl)
+        if parseresult:
+            for name in vars(self).keys():
+                setattr(self, name, parseresult.group(name))
+            return
+        parseresult = re.fullmatch(patterns["scp"], repourl)
+        if parseresult:
+            self.scheme = "ssh"
+            for name in ("username", "hostname", "owner", "reponame"):
+                setattr(self, name, parseresult.group(name))
+            return
+        parseresult = re.fullmatch(patterns["file"], repourl)
+        if parseresult:
+            self.scheme = "file"
+            self.reponame = parseresult.group("reponame")
--- repomisc/test/test_repo.py
+++ repomisc/test/test_repo.py
@@ -18,8 +18,6 @@ class RepoTest(unittest.TestCase):
              for key, value in data.items() if value is not None})
         repo = Repo()
         repo.urlparse(url)
-        print(vars(repo))
-        print(target)
         self.assertDictEqual(vars(repo), target)
 
     def test_urlparse(self):
@@ -43,8 +41,8 @@ class RepoTest(unittest.TestCase):
                 "reponame": "mlmisc",
             })
         self.assertRepoUrl(
-            "https://ok:rf@github.com/miacro/mlmisc.git", {
-                "scheme": "https",
+            "git://ok:rf@github.com/miacro/mlmisc.git/", {
+                "scheme": "git",
                 "username": "ok",
                 "password": "rf",
                 "hostname": "github.com",
@@ -53,19 +51,62 @@ class RepoTest(unittest.TestCase):
                 "reponame": "mlmisc"
             })
         self.assertRepoUrl(
-            "https://github.com/miacro/mlmisc", {
+            "https://github.com/miacro/mlmisc/", {
                 "scheme": "https",
                 "hostname": "github.com",
                 "owner": "miacro",
                 "reponame": "mlmisc"
             })
         self.assertRepoUrl(
-            "https://a:b@github.com:12345/miacro/mlmisc.git", {
-                "scheme": "https",
+            "ftps://a:b@github.com:12345/miacro/mlmisc.git", {
+                "scheme": "ftps",
                 "username": "a",
                 "password": "b",
                 "hostname": "github.com",
                 "port": "12345",
                 "owner": "miacro",
                 "reponame": "mlmisc"
+            })
+        self.assertRepoUrl("file:///abc/dfe", {
+            "scheme": "file",
+            "reponame": "dfe"
+        })
+        self.assertRepoUrl("file:///abc/def/hij/dddd.git", {
+            "scheme": "file",
+            "reponame": "dddd"
+        })
+        self.assertRepoUrl("/abc/def/hij/dddd", {
+            "scheme": "file",
+            "reponame": "dddd"
+        })
+        self.assertRepoUrl("/abc", {"reponame": "abc", "scheme": "file"})
+        self.assertRepoUrl("/abc.git", {"reponame": "abc", "scheme": "file"})
+        self.assertRepoUrl("/abc/def/../../dddd.git", {
+            "reponame": "dddd",
+            "scheme": "file"
+        })
+        self.assertRepoUrl("abc", {"reponame": "abc", "scheme": "file"})
+        self.assertRepoUrl("./abc/../dfe/daff/dddd.git/", {
+            "reponame": "dddd",
+            "scheme": "file"
+        })
+        self.assertRepoUrl("../ddd/../dfidf/drt/gggagi.git", {
+            "reponame": "gggagi",
+            "scheme": "file"
+        })
+        self.assertRepoUrl(
+            "git@github.com:~/miacro/mlmisc.git", {
+                "reponame": "mlmisc",
+                "username": "git",
+                "hostname": "github.com",
+                "owner": "miacro",
+                "scheme": "ssh",
+            })
+        self.assertRepoUrl(
+            "test@github.com:/abc/def/aaa.git", {
+                "reponame": "aaa",
+                "username": "test",
+                "hostname": "github.com",
+                "owner": "def",
+                "scheme": "ssh",
             })
